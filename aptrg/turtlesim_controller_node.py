fe31357d34ed0a7c2d36179e9b720fdc7d28c3d5
noczero <satrya@zeroinside.id>
2024-11-16 22:37:53 +0700

    feat: add turtle sim square control

--- aptrg/turtlesim_controller_node.py
+++ aptrg/turtlesim_controller_node.py
@@ -5,7 +5,6 @@ from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 
 class TurtlesimControllerNode(Node):
-
     def __init__(self):
         super().__init__("turtlesim_controller_node")
         self.get_logger().info("TurtlesimControllerNode is running...")
@@ -28,66 +27,75 @@ class TurtlesimControllerNode(Node):
 
         self.pose = None
 
-        self.target_points = [(5.5, 5.5), (7.5, 5.5), (7.5, 7.5), (5.5, 7.5), (5.5, 5.5)]
+        self.target_points = [(7.5, 5.5), (7.5, 7.5), (5.5, 7.5), (5.5, 5.5)]
 
-        self.target_heading = [0, 3.14/2, 3.14, -3.14/2]
+        self.target_heading = [90, 180, 270, 0 ]
         
         self.target_index = 0
 
+        self.heading = 0
+
         self.LINEAR_VELOCITY = 0.5
 
+        self.ANGULAR_VELOCITY = 0.05
+
         self.STATE = 'FORWARD'
+
 
     def logger(self):
         return self.get_logger()
 
+
     def cmd_vel_callback(self):
         # main logic guidance
+        if (self.target_index >= len(self.target_points)):
+            self.logger().info("Mission completed")
+            return 
+
         current_point = (self.pose.x, self.pose.y)
         
         target_point = self.target_points[self.target_index]
-
-        #self.logger().info(f"current point {current_point}; target_point {target_point}")
 
         distance = self.euclidean_distance(current_point, target_point)
 
         twist_msg = Twist()
 
-        if (distance <= 0.1):
-            self.logger().info("Target reached..")
-
-            self.target_index += 1
-            
-            self.STATE = "YAW"
-
-            if (self.target_index == 4):
-                self.logger().info("Mission finsihed..")
-                return
-
-         
-
         if self.STATE == "FORWARD":
             twist_msg.linear.x = self.LINEAR_VELOCITY
+        
         else:
-            if (self.target_index != 1 and self.pose.theta < self.target_heading[self.target_index]):
+            if (self.heading <= self.target_heading[self.target_index - 1]):
                 # YAW 
-                twist_msg.angular.z = self.LINEAR_VELOCITY
+                twist_msg.angular.z = self.ANGULAR_VELOCITY
             else:
                 self.STATE = "FORWARD"
 
-           
         self.cmd_vel_publisher_.publish(twist_msg)
+ 
+        if (distance <= 0.1):
+            self.logger().info("Target reached..")
+            self.STATE = "YAW"
+            self.target_index += 1 
+
         self.get_logger().info(f"Distance to target point: {distance}, taget_point: {target_point}, current_point: {current_point}, target_index: {self.target_index}, self_state: {self.STATE}, theta: {self.pose.theta}")
+        self.logger().info(f"heading: {self.heading}, theta: {self.pose.theta}, target_heading: {self.target_heading[self.target_index - 1]}")
 
 
-    
     def pose_listener_callback(self, pose):
         self.pose = pose
+        self.heading = self.radian_to_degree(pose.theta)
 
 
     def euclidean_distance(self, current_point, target_point):
         return math.sqrt((current_point[0]-target_point[0])**2 + (current_point[1] - target_point[1])**2)
 
+
+    def radian_to_degree(self, radian):
+        theta_degree = radian * (180.0 / math.pi)
+        if theta_degree < 0:
+            theta_degree += 360
+
+        return theta_degree
 
 
 def main(args=None):
@@ -100,6 +108,7 @@ def main(args=None):
     turtlesim_controller_node.destory_node()
 
     rclpy.shutdown()
+
 
 if __name__ == "__main__":
     main()
